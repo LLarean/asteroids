fd297f0eb1b6fe5c09e190052d7b471fdc8486ec
Stanislav Kozlov <llarean@ya.ru>
2024-10-08 17:07:39 +0300

    Updating the menu

--- Assets/Asteroids/Scripts/Asteroid.cs
+++ Assets/Asteroids/Scripts/Asteroid.cs
@@ -15,10 +15,12 @@ public class Asteroid : MonoBehaviour
     
     private void OnTriggerEnter2D(Collider2D other)
     {
-        EventBus.RaiseEvent<IGameHandler>(handler => handler.HandleExperienceChange());
-        _rigidbody2D.velocity = Vector2.zero;
-        
-        AudioPlayer.Instance.PlayExplosion();
         gameObject.SetActive(false);
+        _rigidbody2D.velocity = Vector2.zero;
+
+        if (other.TryGetComponent(out Laser laser) == false) return;
+        
+        EventBus.RaiseEvent<IGameHandler>(handler => handler.HandleExperienceChange());
+        AudioPlayer.Instance.PlayExplosion();
     }
 }
